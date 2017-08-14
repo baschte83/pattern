e5dc903372f4095c09d41deb0731bfc71f4d7442
Sebastian Baumann <bauman21@hm.edu>
2017-08-14 16:48:07 +0200

    Correcting the comments and translating them into english. Updating the .gitignore file.

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,26 +1,35 @@
 /**
- * Organisation: Hochschule Muenchen, Fakultaet 07
- * Project: Praktikum Softwareentwicklung 1 Frau Prof. Dr. Hammerschall, WS 2016/2017
- * Author: Sebastian Baumann, bauman21@hm.edu
+ * Organisation: University of applied sciences Munich, faculty 07
+ * Project: lab to class software developement 1 Mrs. Prof. Dr. Hammerschall, WS 2016/2017
+ * Author: Sebastian Baumann, sebastian_baumann@gmx.net
  * Study group: IF3A
  * Date: 10. November 2016
- * Purpose: Loesung Praktikumseinheit 03: Pattern
- * Software: Oracle Java SE JDK 1.8.0_101, Windows 10 x86_64
- * Hardware: Intel Core i5-6300 @ 2.40 GHz 2.50 GHz, 2 Cores, 4096 MB RAM
+ * Purpose: solution to lab Pattern
  */
-
 public class Pattern {
 
+    /**
+     * Main method.
+     * @param args - variable number of values from the terminal.
+     */
     public static void main(String[] args) {
 
-        // Anlegen und initialisieren/berechnen der benoetigten Variablen
-        int size = Integer.parseInt(args[0]);	        // Einlesen der Groesse des Patterns.
-        size = Math.abs(size);					        // eine eventuell negative Pattern-Groesse wird zu einer positiven gemacht
-        int countLines = 0;						        // Zeilenzaehler. Der Zaehler beginnt mit Zeile 1
-        int countCharacters = 0;                        // Zeichenzaehler. Der Zaehler zaehlt die bereits erzeugten Zeichen in jeder Zeile.
-        int counterConterX = size - 1;                  // Zaehler fuer die zweiten X in jeder Zeile. Dieser Zaehler zaehlt rueckwaerts
+        // Variable to store the size of the pattern from the terminal.
+        int size = Integer.parseInt(args[0]);
 
-        // Pruefen, ob die eingegebene Groesse gerade oder 0 ist. Sollte dem so sein, wird eine Fehlermeldung ausgegeben.
+        // Converting a possible negative value for size into a positive one.
+        size = Math.abs(size);
+
+        // Counter to call all lines.
+        int countLines = 0;
+
+        // Counter to count all characters of a line.
+        int countCharacters = 0;
+
+        // Counter for the second X from the right.
+        int counterConterX = size - 1;
+
+        // Check if the size is 0 or not. If size is 0, than an error occurs.
         if ((size == 0) || (size % 2 == 0)) {
 
             System.out.println("Invalid Input");
@@ -28,13 +37,15 @@ public class Pattern {
         }
         else {
 
-            for (int i = 0; i < size; i++) {        // Erzeugt die Zeilen
+            // This loop creates the lines.
+            for (int i = 0; i < size; i++) {
 
-                for (int j = 0; j < size; j++) {    // Erzeugt die Zeichen einer Zeile
+                // This loop creates the characters of a single line.
+                for (int j = 0; j < size; j++) {
 
-                    /**
-                     * Es wird immer dann ein X ausgegeben, wenn entweder die gezaehlten Zeichen dem Zeilenzaehler entsprechen,
-                     * oder wenn die gezaehlten Zeichen dem Zaehler fuer das zweite X in jeder Zeile entsprechen.
+                    /*
+                     * Everytime the counted characters are equal to counted lines or the counted characters are equal to the
+                     * counter of the second X from the right, an X is printed to the screen.
                      */
                     if (countCharacters == countLines || countCharacters == counterConterX) {
 
@@ -42,10 +53,7 @@ public class Pattern {
                         countCharacters++;
 
                     }
-                    /**
-                     * Trifft keine der Bedingungen fuer das Erzeugen eines X zu und sollen aber noch Zeichen in der Zeile
-                     * erzeugt werden, so soll in jedem Fall ein Punkt erzeugt werden.
-                     */
+                    // If no X is printed, than a point should be printed to the screen.
                     else {
 
                         System.out.print('.');
@@ -54,7 +62,8 @@ public class Pattern {
                     }
                 }
 
-                countCharacters = 0;                // Nach jeder erzeugten Zeile muss der Zeichen-Counter zurueckgesetzt werden.
+                // After every counted line the counter for the characters has to be reset.
+                countCharacters = 0;
                 counterConterX--;
                 countLines++;
                 System.out.println();
